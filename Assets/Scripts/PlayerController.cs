ece6e9189c72af1e83eb82dcb6b131d02fa071ee
SilkHood <andres7.7mac@gmail.com>
2024-10-18 23:55:36 -0500

    Movilidad Usuario

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,72 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    float speed = 3.0f;         // Velocidad horizontal
+    float jumpForce = 6.0f;    // Fuerza de salto
+    bool isGround = false; 
+
+    private void Start()
     {
-        CharacterController controller;
-        [SerializeField] float movX;
-
-        float gravityScale = -9.8f;
-        [Range(0, 5)]
-        public float speed;
-        [Range(1, 5)]
-        public int jumpForce;
-        Vector2 movement;
-        [SerializeField] Vector2 gravity;
-
-        [Header("Rotation Variables")]
-
-        public float xRotation; // Para almacenar la rotación en el eje X
-        public float yRotation; // Para almacenar la rotación en el eje Y
-
-
-        void Start()
-        {
-            controller = GetComponent<CharacterController>();
-            Cursor.lockState = CursorLockMode.Locked; // Desaparece el cursor de la pantalla para asegurar un uso más cómodo
-        }
-        void Update()
-        {
-            CallInputs();
-            MovePlayer();
-
-            if (Input.GetButtonDown("Jump") && controller.isGrounded)
-            {
-                JumpPlayer();
-            }
-
-            if (!controller.isGrounded)
-            {
-                ApplyGravity();
-            }
-            else
-            {
-                gravity.y = -2;
-            }
-        }
-        void CallInputs()
-        {
-            movX = Input.GetAxis("Horizontal");
-        }
-
-        void MovePlayer()
-        {
-            movement = transform.right * movX;
-            controller.SimpleMove(movement * speed);
-        }
-        void ApplyGravity()
-        {
-            gravity.y += gravityScale * Time.deltaTime;
-            controller.Move(gravity * Time.deltaTime);
-        }
-
-        void JumpPlayer()
-        {
-            gravity.y = Mathf.Sqrt(jumpForce * -2 * gravityScale);
-            controller.Move(gravity * Time.deltaTime);
-        }
-
+        rigidBody = GetComponent<Rigidbody2D>();
     }
+
+    private void Update()
+    {
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();  // Controlar el movimiento horizontal en cada frame de física
+    }
+
+
+    void Jump()
+    {
+        // Detectar el salto y aplicarlo solo si el personaje está en el suelo
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);  // Saltar verticalmente
+            isGround = false;  // Ya no está en el suelo
+        }
+    }
+
+    void Movement()
+    {
+        // Obtener la entrada horizontal (-1 para izquierda, 1 para derecha, 0 para no moverse)
+        float getX = Input.GetAxis("Horizontal");
+
+        // Aplicar movimiento horizontal constante con la dirección obtenida de las teclas de flecha
+        rigidBody.velocity = new Vector2(getX * speed, rigidBody.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Detectar si el personaje toca el suelo para permitir que vuelva a saltar
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+}
+
 
 
 
