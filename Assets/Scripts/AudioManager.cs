e1f8b115b1a9afaf28a49de38ce97b3840027d33
Adriana-94 <adrianakalderon94@gmail.com>
2024-10-20 20:20:45 -0500

    Music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
-
-    public AudioSource audio;
-    // Start is called before the first frame update
-    void Start()
-    {
-        audio = GetComponent<AudioSource>();
-    }
-
+    [SerializeField] AudioSource musicSource, effectsSource;
+    public AudioMixer master;
     private void Awake()
     {
         if (instance != null)
@@ -25,10 +20,15 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    void ReproducirAudio(AudioClip clip)
+    public void PlaySFX(AudioClip clip)
     {
-        // Solo se ejecuta una sola vez el clip de audio
-        audio.PlayOneShot(clip);
+        effectsSource.PlayOneShot(clip);
+    }
+    public void ReproducirAudio(AudioClip music)
+    {
+        musicSource.Stop();
+        musicSource.clip = music;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 }
